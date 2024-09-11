b0cd579b412210b2bef70f1899d76e2e119023e2
Timodruid <dxleotimox100pre@gmail.com>
2024-09-11 19:46:51 -0400

    Agregar archivos de proyecto.

---
+++ PruebaMS/Main.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+using System.Collections.Generic;
+
+namespace PruebaMS
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.panelSideMenu = new System.Windows.Forms.TabPage();
+            this.materialCard1 = new MaterialSkin.Controls.MaterialCard();
+            this.materialRadioButton2 = new MaterialSkin.Controls.MaterialRadioButton();
+            this.materialRadioButton1 = new MaterialSkin.Controls.MaterialRadioButton();
+            this.materialButton1 = new MaterialSkin.Controls.MaterialButton();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.materialTabControl1.SuspendLayout();
+            this.panelSideMenu.SuspendLayout();
+            this.materialCard1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // materialTabControl1
+            // 
+            this.materialTabControl1.Controls.Add(this.tabPage3);
+            this.materialTabControl1.Controls.Add(this.panelSideMenu);
+            this.materialTabControl1.Controls.Add(this.tabPage1);
+            this.materialTabControl1.Controls.Add(this.tabPage2);
+            this.materialTabControl1.Depth = 0;
+            this.materialTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.materialTabControl1.Location = new System.Drawing.Point(3, 64);
+            this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl1.Multiline = true;
+            this.materialTabControl1.Name = "materialTabControl1";
+            this.materialTabControl1.SelectedIndex = 0;
+            this.materialTabControl1.Size = new System.Drawing.Size(903, 496);
+            this.materialTabControl1.TabIndex = 1;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(895, 470);
+            this.tabPage3.TabIndex = 3;
+            this.tabPage3.Text = "INICIO";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // panelSideMenu
+            // 
+            this.panelSideMenu.Controls.Add(this.materialCard1);
+            this.panelSideMenu.Location = new System.Drawing.Point(4, 22);
+            this.panelSideMenu.Name = "panelSideMenu";
+            this.panelSideMenu.Padding = new System.Windows.Forms.Padding(3);
+            this.panelSideMenu.Size = new System.Drawing.Size(895, 470);
+            this.panelSideMenu.TabIndex = 0;
+            this.panelSideMenu.Text = "CONFIGURACION";
+            this.panelSideMenu.UseVisualStyleBackColor = true;
+            // 
+            // materialCard1
+            // 
+            this.materialCard1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.materialCard1.Controls.Add(this.materialRadioButton2);
+            this.materialCard1.Controls.Add(this.materialRadioButton1);
+            this.materialCard1.Controls.Add(this.materialButton1);
+            this.materialCard1.Depth = 0;
+            this.materialCard1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialCard1.Location = new System.Drawing.Point(90, 17);
+            this.materialCard1.Margin = new System.Windows.Forms.Padding(14);
+            this.materialCard1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialCard1.Name = "materialCard1";
+            this.materialCard1.Padding = new System.Windows.Forms.Padding(14);
+            this.materialCard1.Size = new System.Drawing.Size(699, 293);
+            this.materialCard1.TabIndex = 0;
+            // 
+            // materialRadioButton2
+            // 
+            this.materialRadioButton2.AutoSize = true;
+            this.materialRadioButton2.Depth = 0;
+            this.materialRadioButton2.Location = new System.Drawing.Point(397, 98);
+            this.materialRadioButton2.Margin = new System.Windows.Forms.Padding(0);
+            this.materialRadioButton2.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialRadioButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRadioButton2.Name = "materialRadioButton2";
+            this.materialRadioButton2.Ripple = true;
+            this.materialRadioButton2.Size = new System.Drawing.Size(145, 37);
+            this.materialRadioButton2.TabIndex = 4;
+            this.materialRadioButton2.TabStop = true;
+            this.materialRadioButton2.Text = "ImprimirPorRed";
+            this.materialRadioButton2.UseVisualStyleBackColor = true;
+            // 
+            // materialRadioButton1
+            // 
+            this.materialRadioButton1.AutoSize = true;
+            this.materialRadioButton1.Depth = 0;
+            this.materialRadioButton1.Location = new System.Drawing.Point(65, 98);
+            this.materialRadioButton1.Margin = new System.Windows.Forms.Padding(0);
+            this.materialRadioButton1.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialRadioButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRadioButton1.Name = "materialRadioButton1";
+            this.materialRadioButton1.Ripple = true;
+            this.materialRadioButton1.Size = new System.Drawing.Size(158, 37);
+            this.materialRadioButton1.TabIndex = 3;
+            this.materialRadioButton1.TabStop = true;
+            this.materialRadioButton1.Text = "ImprimirPorCable";
+            this.materialRadioButton1.UseVisualStyleBackColor = true;
+            this.materialRadioButton1.CheckedChanged += new System.EventHandler(this.materialRadioButton1_CheckedChanged);
+            // 
+            // materialButton1
+            // 
+            this.materialButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialButton1.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.materialButton1.Depth = 0;
+            this.materialButton1.HighEmphasis = true;
+            this.materialButton1.Icon = ((System.Drawing.Image)(resources.GetObject("materialButton1.Icon")));
+            this.materialButton1.Location = new System.Drawing.Point(228, 174);
+            this.materialButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialButton1.Name = "materialButton1";
+            this.materialButton1.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.materialButton1.Size = new System.Drawing.Size(186, 36);
+            this.materialButton1.TabIndex = 2;
+            this.materialButton1.Text = "MaterialButton1";
+            this.materialButton1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.materialButton1.UseAccentColor = false;
+            this.materialButton1.UseVisualStyleBackColor = true;
+            this.materialButton1.Click += new System.EventHandler(this.materialButton1_Click);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(895, 470);
+            this.tabPage1.TabIndex = 1;
+            this.tabPage1.Text = "CALIFICAR";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(895, 470);
+            this.tabPage2.TabIndex = 2;
+            this.tabPage2.Text = "OBTENER HOJA";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 563);
+            this.Controls.Add(this.materialTabControl1);
+            this.DrawerTabControl = this.materialTabControl1;
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.materialTabControl1.ResumeLayout(false);
+            this.panelSideMenu.ResumeLayout(false);
+            this.materialCard1.ResumeLayout(false);
+            this.materialCard1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private System.Windows.Forms.TabPage panelSideMenu;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private MaterialSkin.Controls.MaterialButton materialButton1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private MaterialSkin.Controls.MaterialCard materialCard1;
+        private MaterialSkin.Controls.MaterialRadioButton materialRadioButton2;
+        private MaterialSkin.Controls.MaterialRadioButton materialRadioButton1;
+    }
+}
+
---
+++ PruebaMS/Main.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WIA;
+namespace PruebaMS
+{
+    public partial class Main : MaterialSkin.Controls.MaterialForm
+    {
+        public List<MaterialSkin.Controls.MaterialButton> devicesButton;
+        public Main()
+        {
+            this.devicesButton = new List<MaterialSkin.Controls.MaterialButton>();
+            getPrinters();
+            InitializeComponent();
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void getPrinters()
+        {
+            try
+            {
+                System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+                // Crear un objeto Device Manager para gestionar dispositivos WIA
+                DeviceManager deviceManager = new DeviceManager();
+                Device device = null;
+                var i = 0;
+
+                // Encontrar un dispositivo WIA
+                foreach (DeviceInfo deviceInfo in deviceManager.DeviceInfos)
+                {
+                    var ap = new MaterialSkin.Controls.MaterialButton();
+                    ap.Text = deviceInfo.Properties["Name"].get_Value().ToString();
+                    ap.Location = new System.Drawing.Point(251, 122 + i * 150);
+                    ap.Icon = ((System.Drawing.Image)(resources.GetObject("materialButton1.Icon")));
+                    devicesButton.Add(ap);
+                    this.Controls.Add(ap);
+                    i++;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+        private void materialButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialRadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            var button1 = new MaterialSkin.Controls.MaterialButton();
+            this.Hide();
+
+            // Configurar el botón
+            button1.Text = "Nuevo Botón";
+            button1.Location = new Point(100, 100); // Ubicación del botón
+            button1.Size = new Size(100, 40); // Tamaño del botón
+
+            // Añadir el botón al formulario
+            this.Controls.Add(button1);
+            button1.Show();
+
+            this.Show(); // Mostrar el formulario nuevamente si lo deseas
+        }
+    }
+}
