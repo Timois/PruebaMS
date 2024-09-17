fbb0e30789343705b30d2d258e9112837850b4f4
Timodruid <dxleotimox100pre@gmail.com>
2024-09-17 18:10:34 -0400

    cambios de botnes a listbox

--- PruebaMS/Main.Designer.cs
+++ PruebaMS/Main.Designer.cs
@@ -31,25 +31,28 @@ namespace PruebaMS
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
             this.tabPage3 = new System.Windows.Forms.TabPage();
-            this.panelSideMenu = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
             this.materialCard1 = new MaterialSkin.Controls.MaterialCard();
-            this.materialRadioButton2 = new MaterialSkin.Controls.MaterialRadioButton();
-            this.materialRadioButton1 = new MaterialSkin.Controls.MaterialRadioButton();
-            this.materialButton1 = new MaterialSkin.Controls.MaterialButton();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.buttonCable = new System.Windows.Forms.Button();
+            this.buttonRed = new System.Windows.Forms.Button();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.materialTabControl1.SuspendLayout();
-            this.panelSideMenu.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
             this.materialCard1.SuspendLayout();
             this.SuspendLayout();
             // 
             // materialTabControl1
             // 
             this.materialTabControl1.Controls.Add(this.tabPage3);
-            this.materialTabControl1.Controls.Add(this.panelSideMenu);
+            this.materialTabControl1.Controls.Add(this.tabPage4);
             this.materialTabControl1.Controls.Add(this.tabPage1);
             this.materialTabControl1.Controls.Add(this.tabPage2);
             this.materialTabControl1.Depth = 0;
@@ -64,6 +67,7 @@ namespace PruebaMS
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.label1);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
@@ -72,23 +76,33 @@ namespace PruebaMS
             this.tabPage3.Text = "INICIO";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
-            // panelSideMenu
+            // label1
             // 
-            this.panelSideMenu.Controls.Add(this.materialCard1);
-            this.panelSideMenu.Location = new System.Drawing.Point(4, 22);
-            this.panelSideMenu.Name = "panelSideMenu";
-            this.panelSideMenu.Padding = new System.Windows.Forms.Padding(3);
-            this.panelSideMenu.Size = new System.Drawing.Size(895, 470);
-            this.panelSideMenu.TabIndex = 0;
-            this.panelSideMenu.Text = "CONFIGURACION";
-            this.panelSideMenu.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.materialCard1);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(895, 470);
+            this.tabPage4.TabIndex = 0;
+            this.tabPage4.Text = "CONFIGURACION";
+            this.tabPage4.UseVisualStyleBackColor = true;
             // 
             // materialCard1
             // 
             this.materialCard1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.materialCard1.Controls.Add(this.materialRadioButton2);
-            this.materialCard1.Controls.Add(this.materialRadioButton1);
-            this.materialCard1.Controls.Add(this.materialButton1);
+            this.materialCard1.Controls.Add(this.listBox3);
+            this.materialCard1.Controls.Add(this.listBox2);
+            this.materialCard1.Controls.Add(this.listBox1);
+            this.materialCard1.Controls.Add(this.buttonCable);
+            this.materialCard1.Controls.Add(this.buttonRed);
             this.materialCard1.Depth = 0;
             this.materialCard1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.materialCard1.Location = new System.Drawing.Point(90, 17);
@@ -96,61 +110,52 @@ namespace PruebaMS
             this.materialCard1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialCard1.Name = "materialCard1";
             this.materialCard1.Padding = new System.Windows.Forms.Padding(14);
-            this.materialCard1.Size = new System.Drawing.Size(699, 293);
+            this.materialCard1.Size = new System.Drawing.Size(763, 453);
             this.materialCard1.TabIndex = 0;
             // 
-            // materialRadioButton2
+            // listBox3
             // 
-            this.materialRadioButton2.AutoSize = true;
-            this.materialRadioButton2.Depth = 0;
-            this.materialRadioButton2.Location = new System.Drawing.Point(397, 98);
-            this.materialRadioButton2.Margin = new System.Windows.Forms.Padding(0);
-            this.materialRadioButton2.MouseLocation = new System.Drawing.Point(-1, -1);
-            this.materialRadioButton2.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialRadioButton2.Name = "materialRadioButton2";
-            this.materialRadioButton2.Ripple = true;
-            this.materialRadioButton2.Size = new System.Drawing.Size(145, 37);
-            this.materialRadioButton2.TabIndex = 4;
-            this.materialRadioButton2.TabStop = true;
-            this.materialRadioButton2.Text = "ImprimirPorRed";
-            this.materialRadioButton2.UseVisualStyleBackColor = true;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(497, 217);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(203, 134);
+            this.listBox3.TabIndex = 7;
             // 
-            // materialRadioButton1
+            // listBox2
             // 
-            this.materialRadioButton1.AutoSize = true;
-            this.materialRadioButton1.Depth = 0;
-            this.materialRadioButton1.Location = new System.Drawing.Point(65, 98);
-            this.materialRadioButton1.Margin = new System.Windows.Forms.Padding(0);
-            this.materialRadioButton1.MouseLocation = new System.Drawing.Point(-1, -1);
-            this.materialRadioButton1.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialRadioButton1.Name = "materialRadioButton1";
-            this.materialRadioButton1.Ripple = true;
-            this.materialRadioButton1.Size = new System.Drawing.Size(158, 37);
-            this.materialRadioButton1.TabIndex = 3;
-            this.materialRadioButton1.TabStop = true;
-            this.materialRadioButton1.Text = "ImprimirPorCable";
-            this.materialRadioButton1.UseVisualStyleBackColor = true;
-            this.materialRadioButton1.CheckedChanged += new System.EventHandler(this.materialRadioButton1_CheckedChanged);
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(497, 57);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(203, 147);
+            this.listBox2.TabIndex = 6;
             // 
-            // materialButton1
+            // listBox1
             // 
-            this.materialButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.materialButton1.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
-            this.materialButton1.Depth = 0;
-            this.materialButton1.HighEmphasis = true;
-            this.materialButton1.Icon = ((System.Drawing.Image)(resources.GetObject("materialButton1.Icon")));
-            this.materialButton1.Location = new System.Drawing.Point(228, 174);
-            this.materialButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
-            this.materialButton1.MouseState = MaterialSkin.MouseState.HOVER;
-            this.materialButton1.Name = "materialButton1";
-            this.materialButton1.NoAccentTextColor = System.Drawing.Color.Empty;
-            this.materialButton1.Size = new System.Drawing.Size(186, 36);
-            this.materialButton1.TabIndex = 2;
-            this.materialButton1.Text = "MaterialButton1";
-            this.materialButton1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
-            this.materialButton1.UseAccentColor = false;
-            this.materialButton1.UseVisualStyleBackColor = true;
-            this.materialButton1.Click += new System.EventHandler(this.materialButton1_Click);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(64, 69);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(213, 212);
+            this.listBox1.TabIndex = 5;
+            // 
+            // buttonCable
+            // 
+            this.buttonCable.Location = new System.Drawing.Point(292, 69);
+            this.buttonCable.Name = "buttonCable";
+            this.buttonCable.Size = new System.Drawing.Size(126, 23);
+            this.buttonCable.TabIndex = 4;
+            this.buttonCable.Text = "ImpresorasPorCable";
+            this.buttonCable.UseVisualStyleBackColor = true;
+            this.buttonCable.Click += new System.EventHandler(this.buttonCable_Click);
+            // 
+            // buttonRed
+            // 
+            this.buttonRed.Location = new System.Drawing.Point(292, 146);
+            this.buttonRed.Name = "buttonRed";
+            this.buttonRed.Size = new System.Drawing.Size(111, 32);
+            this.buttonRed.TabIndex = 3;
+            this.buttonRed.Text = "ImpresorasPorRed";
+            this.buttonRed.UseVisualStyleBackColor = true;
+            this.buttonRed.Click += new System.EventHandler(this.buttonRed_Click);
             // 
             // tabPage1
             // 
@@ -182,23 +187,27 @@ namespace PruebaMS
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.materialTabControl1.ResumeLayout(false);
-            this.panelSideMenu.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
             this.materialCard1.ResumeLayout(false);
-            this.materialCard1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
-        private System.Windows.Forms.TabPage panelSideMenu;
+        private System.Windows.Forms.TabPage tabPage4;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
-        private MaterialSkin.Controls.MaterialButton materialButton1;
         private System.Windows.Forms.TabPage tabPage3;
         private MaterialSkin.Controls.MaterialCard materialCard1;
-        private MaterialSkin.Controls.MaterialRadioButton materialRadioButton2;
-        private MaterialSkin.Controls.MaterialRadioButton materialRadioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button buttonCable;
+        private System.Windows.Forms.Button buttonRed;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox2;
     }
 }
 
--- PruebaMS/Main.cs
+++ PruebaMS/Main.cs
@@ -8,70 +8,123 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WIA;
+using System.Management;
+using MaterialSkin.Controls;
+using System.Printing;
+
 namespace PruebaMS
 {
     public partial class Main : MaterialSkin.Controls.MaterialForm
     {
-        public List<MaterialSkin.Controls.MaterialButton> devicesButton;
         public Main()
         {
-            this.devicesButton = new List<MaterialSkin.Controls.MaterialButton>();
-            getPrinters();
             InitializeComponent();
-            
+            ListConnectedScanners();
         }
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-        private void getPrinters()
+        private void ListConnectedScanners()
         {
             try
             {
-                System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
-                // Crear un objeto Device Manager para gestionar dispositivos WIA
+                // Crear un objeto de DeviceManager para manejar los dispositivos WIA
                 DeviceManager deviceManager = new DeviceManager();
-                Device device = null;
-                var i = 0;
 
-                // Encontrar un dispositivo WIA
-                foreach (DeviceInfo deviceInfo in deviceManager.DeviceInfos)
+                // Limpiar el ListBox antes de agregar elementos
+                listBox1.Items.Clear();
+
+                // Iterar a través de los dispositivos conectados
+                for (int i = 1; i <= deviceManager.DeviceInfos.Count; i++)
                 {
-                    var ap = new MaterialSkin.Controls.MaterialButton();
-                    ap.Text = deviceInfo.Properties["Name"].get_Value().ToString();
-                    ap.Location = new System.Drawing.Point(251, 122 + i * 150);
-                    ap.Icon = ((System.Drawing.Image)(resources.GetObject("materialButton1.Icon")));
-                    devicesButton.Add(ap);
-                    this.Controls.Add(ap);
-                    i++;
+                    DeviceInfo deviceInfo = deviceManager.DeviceInfos[i];
+
+                    // Verificar si el dispositivo es un escáner
+                    if (deviceInfo.Type == WiaDeviceType.ScannerDeviceType)
+                    {
+                        // Agregar el nombre del escáner al ListBox
+                        listBox1.Items.Add(deviceInfo.Properties["Name"].get_Value());
+                    }
                 }
-                
+
+                // Verificar si no se encontraron escáneres
+                if (listBox1.Items.Count == 0)
+                {
+                    listBox1.Items.Add("No hay impresoras o escáners conectados.");
+                }
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error: {ex.Message}");
+                MessageBox.Show("Error al listar escáneres: " + ex.Message);
             }
         }
-        private void materialButton1_Click(object sender, EventArgs e)
-        {
 
+        private void buttonCable_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ManagementClass printers = new ManagementClass("Win32_SerialPort");
+                ManagementObjectCollection portCollection = printers.GetInstances();
+
+                foreach (ManagementObject port in portCollection)
+                {
+                    string nombrePuerto = port["Name"].ToString();
+                    string estadoPuerto = port["Status"].ToString();
+                    string velocidad = port["Speed"].ToString();
+                    string protocolo = port["Protocol"].ToString();
+
+                    // Filtrar solo puertos serie asociados a impresoras
+                    if (port["Manufacturer"].ToString().ToLower().Contains("hp") ||
+                        port["Manufacturer"].ToString().ToLower().Contains("epson") ||
+                        port["Manufacturer"].ToString().ToLower().Contains("canon"))
+                    {
+                        listBox2.Items.Add($"{nombrePuerto} - Velocidad: {velocidad} bps, Protocolo: {protocolo}, Estado: {estadoPuerto}");
+                    }
+                }
+
+                // Verificar si no se encontraron impresoras locales
+                if (listBox2.Items.Count == 0)
+                {
+                    listBox2.Items.Add("No hay impresoras locales conectadas.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al listar impresoras: " + ex.Message);
+            }
         }
 
-        private void materialRadioButton1_CheckedChanged(object sender, EventArgs e)
+        private void buttonRed_Click(object sender, EventArgs e)
         {
-            var button1 = new MaterialSkin.Controls.MaterialButton();
-            this.Hide();
+            try
+            {
+                ManagementClass printers = new ManagementClass("Win32_Printer");
+                ManagementObjectCollection printerCollection = printers.GetInstances();
 
-            // Configurar el botón
-            button1.Text = "Nuevo Botón";
-            button1.Location = new Point(100, 100); // Ubicación del botón
-            button1.Size = new Size(100, 40); // Tamaño del botón
+                foreach (ManagementObject printer in printerCollection)
+                {
+                    string nombreImpresora = printer["Name"].ToString();
+                    string estadoImpresora = printer["Status"].ToString();
 
-            // Añadir el botón al formulario
-            this.Controls.Add(button1);
-            button1.Show();
+                    // Filtrar solo impresoras conectadas por red
+                    if (printer["Network" as object] != null && (bool)printer["Network"])
+                    {
+                        listBox3.Items.Add($"{nombreImpresora} ({estadoImpresora})");
+                    }
+                }
 
-            this.Show(); // Mostrar el formulario nuevamente si lo deseas
+                // Verificar si no se encontraron impresoras de red
+                if (listBox3.Items.Count == 0)
+                {
+                    listBox3.Items.Add("No hay impresoras de red conectadas.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al listar impresoras de red: " + ex.Message);
+            }
         }
     }
 }
+    
